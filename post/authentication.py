d49f65ce71f5ef029073ac5032b0e5c507cb54e5
qihui <qihui.jia@mods.se>
2025-05-04 22:06:19 +0200

    Query UserToken with token, is_active and expires_at to make sure to get the valid token

--- post/authentication.py
+++ post/authentication.py
@@ -1,5 +1,6 @@
 from ninja.security import HttpBearer
 from .models import UserToken
+from django.utils import timezone
 
 
 class APIAuthBearer(HttpBearer):
@@ -9,7 +10,7 @@ class APIAuthBearer(HttpBearer):
         # Implement your authentication logic here
         # For example, you might want to check if the token is valid
         # and return the corresponding user object.
-        user_token = UserToken.objects.filter(token=token).first()
+        user_token = UserToken.objects.filter(token=token, is_active=True, expires_at__gt=timezone.now()).first()
         if user_token:
             app_user = user_token.user
         return app_user
